ed28bceae6447db20bed23233fcfeb9144c25cca
João Dias <joaoeduardodias123@gmail.com>
2025-04-23 07:57:14 -0300

    fix: resolve error connection

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,40 @@ import { querySummaryInstruments } from "./query-summary-instruments";
 import { setSaveData } from "./services/set-saved-data-in-db";
 
 const wss = new WebSocket.Server({ port: 8080, host: "0.0.0.0" });
-
 wss.on("connection", (ws) => {
-  console.log("connected client");
+  console.log("Connected client");
 
   querySummaryInstruments(ws);
 
   const intervalData = setInterval(() => {
-    querySummaryInstruments(ws);
-  }, 10000); // 10 seconds - seu envio de dados normal
+    if (ws.readyState === WebSocket.OPEN) {
+      querySummaryInstruments(ws);
+    }
+  }, 10000); // 10 segundos
 
-  // PING MANUAL
   const intervalPing = setInterval(() => {
     if (ws.readyState === WebSocket.OPEN) {
       ws.ping();
+      console.log("Ping enviado para cliente");
     }
-  }, 30000); // a cada 30 segundos, envia um ping para manter vivo
+  }, 15000); // 15 segundos (ping separado para manter vivo)
 
   ws.on("pong", () => {
-    // Cliente respondeu o ping
     console.log("Pong recebido do cliente");
   });
 
   ws.on("close", () => {
     clearInterval(intervalData);
     clearInterval(intervalPing);
-    console.log("disconnected client");
+    console.log("Disconnected client");
   });
 
-  ws.on("error", (error) => {
-    console.error("WebSocket erro:", error);
+  ws.on("error", (err) => {
+    console.error("Erro no WebSocket:", err.message);
   });
 });
-
 console.log(
   "Service running: temperatures and new instruments saved to PostgreSQL database every 10 seconds."
 );
+
 setInterval(setSaveData, 10000);
