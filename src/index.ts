b8f3c7d7a6acfd18c92c223eb1c1f0c111ff2fd4
João Dias <joaoeduardodias123@gmail.com>
2025-04-25 10:23:44 -0300

    fix: remove ping

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,7 @@ import { setSaveData } from "./services/set-saved-data-in-db";
 const wss = new WebSocket.Server({ port: 8080, host: "0.0.0.0" });
 wss.on("connection", (ws) => {
   console.log("Connected client");
-  ws.send(
-    JSON.stringify({
-      type: "ping",
-      payload: "Hello! WebSocket is alive!",
-    })
-  );
-  console.log("Mensagem teste enviada imediatamente");
+
   querySummaryInstruments(ws);
 
   const intervalData = setInterval(() => {
@@ -20,20 +14,8 @@ wss.on("connection", (ws) => {
     }
   }, 10000); // 10 segundos
 
-  const intervalPing = setInterval(() => {
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.ping();
-      console.log("Ping enviado para cliente");
-    }
-  }, 15000); // 15 segundos (ping separado para manter vivo)
-
-  ws.on("pong", () => {
-    console.log("Pong recebido do cliente");
-  });
-
   ws.on("close", () => {
     clearInterval(intervalData);
-    clearInterval(intervalPing);
     console.log("Disconnected client");
   });
 
